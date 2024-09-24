d8caa42b3f64430d9a4e297600d5c96bcae3a24b
G3rze <g.o.b.r477@gmail.com>
2024-09-24 14:44:26 -0600

    /[Taller_Parcial]/ Add: mi parcial

---
+++ resources/scripts/button.js
@@ -0,0 +1,5 @@
+// Adds an event listener to the button with the ID "buy-button"
+document.getElementById("buy-button").addEventListener("click", function() {
+    // Shows an alert message when the button is clicked
+    alert("You have successfully bought THE BEST electric toothbrush! \n We have your data now :)");
+});
